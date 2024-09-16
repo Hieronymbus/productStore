637649f4e80db9d44e6e3e47415ffd9189726b57
alex birdsall <birdsallalexander1990@gmail.com>
2024-09-16 15:13:36 +1200

    navbar done and createPage almost done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
-
+import { Container, Flex, Text, Link, HStack, Button, useColorMode } from '@chakra-ui/react'
+import { PlusSquareIcon } from "@chakra-ui/icons"
+import { IoMoon } from "react-icons/io5";
+import { LuSun } from "react-icons/lu";
 const NavBar = () => {
+  
+  const { colorMode, toggleColorMode} = useColorMode()
+  
   return (
-    <div>NavBar</div>
+    <Container >
+      <Flex
+        h={16}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        flexDir={{
+          base:"column",
+          sm:"row"
+        }}
+      >
+        <Text
+          fontSize={{ base: "22", sm: "28" }}
+					fontWeight={"bold"}
+					textTransform={"uppercase"}
+					textAlign={"center"}
+					bgGradient={"linear(to-r, cyan.400, blue.500)"}
+					bgClip={"text"}
+        >
+          <Link to={"/"}>Product Store 🛒</Link>
+        </Text>
+        <HStack spacing={2} alignItems={"center"}>
+          <Link to={"/create"}>
+            <Button>
+              <PlusSquareIcon fontSize={20} />
+            </Button>
+          </Link>
+          <Button onClick={toggleColorMode}>
+            {colorMode === "light" ? <IoMoon /> : <LuSun size='20' />}
+          </Button>
+        </HStack>
+      </Flex>
+    </Container>
   )
 }
 
--- info.md
+++ info.md
@@ -1,3 +1,5 @@
+<<https://www.youtube.com/watch?v=O3BUHwfHf84&ab_channel=freeCodeCamp.org>>
+
 POSTMAN: MOCK FRONT END TO TEST SERVER AND DATABSE CONECTIONS. TEST MODEL CREATION COLECTIONS DOCS
 
 <<BACKEND>>
@@ -12,4 +14,6 @@ POSTMAN: MOCK FRONT END TO TEST SERVER AND DATABSE CONECTIONS. TEST MODEL CREATI
 
 -VITE REACT
 -REACT ROUTER DOM "single page app" with multple rout pages
+-REACT-ICONS: icon library
+-chakra-ui/icons: icon library from chakra
 -CHAKRAUI: Component Library for faster production
