41e526cfd3e638149bf345414a87aa06ccb71733
alex birdsall <birdsallalexander1990@gmail.com>
2024-09-21 16:37:15 +1200

    project completed

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express, { request, response } from "express";
 import dotenv from "dotenv";
+import path from "path"
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js"
 
@@ -7,10 +8,29 @@ dotenv.config()
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
 
 app.use(express.json()); //alows us to accept JSON data in the request.body
    
 app.use("/api/products", productRoutes)
+
+// Check if the app is running in "production" mode
+// "process.env.NODE_ENV" is an environment variable that stores the current mode (development or production)
+if (process.env.NODE_ENV === "production") {
+
+    // Serve static files from the "dist" folder inside the "frontend" directory
+    // In production, the frontend files (HTML, CSS, JavaScript, etc.) are often bundled and placed in a "dist" folder
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    // Handle all other routes by sending the "index.html" file
+    // The "*" means that any route that doesn't match an API or static file will be handled by this
+    app.get("*", (request, response) => {
+        // Send the "index.html" file located in the "dist" folder
+        // This ensures that the frontend's main HTML file is served for any route (like React or Vue's client-side routing)
+        response.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+}
 
 app.listen(PORT, ()=> {
     connectDB();
